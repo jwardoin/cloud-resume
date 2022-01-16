5c1e66ac538e44e60f21d63b4d254bef7d58a2a6
Jordan Ardoin <88734991+jwardoin@users.noreply.github.com>
2022-01-16 11:57:43 -0600

    workflow test 2

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -2,5 +2,5 @@
 async function visitorCounter() {
     let n = await fetch('https://o88fs9koy5.execute-api.us-east-1.amazonaws.com/prod/WebsiteVisits');
     let count = await n.text();
-    document.getElementById('CounterVisitor').innerHTML = "You Are Visitor Number: " + count.slice(1, -1) + "!";
+    document.getElementById('CounterVisitor').innerHTML = "You Are Visitor Number " + count.slice(1, -1) + "!";
 }
