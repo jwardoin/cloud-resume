b9daf287ee1b667c58ee23b8e106006005ae6e48
Jordan Ardoin <88734991+jwardoin@users.noreply.github.com>
2022-02-02 09:04:17 -0600

    Add files via upload

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,6 +1,7 @@
 async function visitorCounter() {
     const url = 'https://o88fs9koy5.execute-api.us-east-1.amazonaws.com/prod/WebsiteVisits';
     const counter = document.querySelector('#CounterVisitor');
+    counter.innerText = "initializing visitor counter..."
     
     const n = await fetch(url);
     const count = await n.text();
