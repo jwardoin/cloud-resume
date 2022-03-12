786c117ead7c76dbcbe52fe96615165f1a0dd1da
Jordan Ardoin <88734991+jwardoin@users.noreply.github.com>
2022-03-11 19:31:41 -0600

    added localStorage

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,9 +1,11 @@
 async function visitorCounter() {
-    const url = 'https://o88fs9koy5.execute-api.us-east-1.amazonaws.com/prod/WebsiteVisits';
     const counter = document.querySelector('#CounterVisitor');
     counter.innerText = "initializing visitor counter..."
-    
-    const n = await fetch(url);
-    const count = await n.text();
-    counter.innerText = `You Are Visitor Number ${count.slice(1, -1)}!`;
+    if(!localStorage.getItem("Visitor Number")) { // checks to see if visitor has already been assigned a number
+        const url = 'https://o88fs9koy5.execute-api.us-east-1.amazonaws.com/prod/WebsiteVisits';
+        const data = await fetch(url);
+        const count = await data.text();
+        localStorage.setItem("Visitor Number", count.slice(1, -1)) // adds their number to localStorage
+    } 
+    counter.innerText = `You Are Visitor Number ${localStorage.getItem("Visitor Number")}!` 
 }
